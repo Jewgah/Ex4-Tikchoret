97116d37da50805cf92e44079b49e669917df8d8
Jewgah <jordanlloydperez@gmail.com>
2021-01-12 20:43:06 +0200

    first commit

---
+++ cmake-build-debug/CMakeFiles/Ex4_Tikchoret.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Ex4_Tikchoret.dir/main.c.obj"
+  "Ex4_Tikchoret.exe"
+  "Ex4_Tikchoret.exe.manifest"
+  "Ex4_Tikchoret.pdb"
+  "libEx4_Tikchoret.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Ex4_Tikchoret.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
